532ea1dbc10e7dc98fb6fd0caa12c8d63db57f7f
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-05-29 19:16:03 +0700

    Membuat class tanpa pewarisan atau tidak boleh diwariskan

---
+++ noPewarisan/noPewarisan.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class baseClass final {
+public:
+	virtual void perkenalan() {
+		cout << "Hallo saya function dari baseClass";
+	}
+};
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya function dari derivedClass";
+	}
+};
